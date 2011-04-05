adb0fa400c8b9838f8066d02720d2b3c90674840
Tobias Munk <schmunk@usrbin.de>
2011-04-05 21:37:41 +0200

    fixing structure

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -33,7 +33,7 @@
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
-				array('label'=>'Admin', 'url'=>array('/p3admin'), 'visible'=>Yii::app()->user->checkAccess('Admin')),
+				array('label'=>'Admin', 'url'=>array('/p3admin')),
 			),
 		)); ?>
 	</div><!-- mainmenu -->
