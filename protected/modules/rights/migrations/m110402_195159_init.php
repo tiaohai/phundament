d62e417b7e6511fe2a323a7d6e78e40df30eab20
Tobias Munk <schmunk@usrbin.de>
2011-04-11 17:33:53 +0200

    working intelude commit

--- protected/modules/rights/migrations/m110402_195159_init.php
+++ protected/modules/rights/migrations/m110402_195159_init.php
@@ -7,60 +7,68 @@ class m110402_195159_init extends CDbMigration {
 		echo "*** WARNING ***\nJust for debugging - Primary Key definitions missing!\n\n";
 
 		$this->createTable("AuthAssignment", array(
-			"itemname" => "varchar(64) not null",
-			"userid" => "varchar(64) not null",
+			"itemname" => "varchar(64)  NOT NULL",
+			"userid" => "varchar(64)  NOT NULL",
 			"bizrule" => "text",
 			"data" => "text",
-			), "");
+			"primary key (itemname,userid)",
+			"foreign key (itemname) references AuthItem (name) on delete cascade on update cascade"
+		));
 
 		$this->insert("AuthAssignment", array(
 			"itemname" => "Admin",
 			"userid" => "2",
-			"bizrule" => "",
+			"bizrule" => null,
 			"data" => "N;",
 			));
 
 		$this->createTable("AuthItem", array(
-			"name" => "varchar(64) not null",
+			"name" => "varchar(64)  NOT NULL",
 			"type" => "integer NOT NULL",
 			"description" => "text",
 			"bizrule" => "text",
 			"data" => "text",
+			"primary key (name)"
 			), "");
 
 		$this->insert("AuthItem", array(
 			"name" => "Admin",
 			"type" => "2",
-			"description" => "",
-			"bizrule" => "",
+			"description" => null,
+			"bizrule" => null,
 			"data" => "N;",
 			));
 
 		$this->insert("AuthItem", array(
 			"name" => "Authenticated",
 			"type" => "2",
-			"description" => "",
-			"bizrule" => "",
+			"description" => null,
+			"bizrule" => null,
 			"data" => "N;",
 			));
 
 		$this->insert("AuthItem", array(
 			"name" => "Guest",
 			"type" => "2",
-			"description" => "",
-			"bizrule" => "",
+			"description" => null,
+			"bizrule" => null,
 			"data" => "N;",
 			));
 
 		$this->createTable("AuthItemChild", array(
-			"parent" => "varchar(64) not null",
-			"child" => "varchar(64) not null",
+			"parent" => "varchar(64)  NOT NULL",
+			"child" => "varchar(64)  NOT NULL",
+			"primary key (parent,child)",
+			"foreign key (parent) references AuthItem (name) on delete cascade on update cascade",
+			"foreign key (child) references AuthItem (name) on delete cascade on update cascade"
 			), "");
 
 		$this->createTable("Rights", array(
-			"itemname" => "varchar(64) not null",
+			"itemname" => "varchar(64)  NOT NULL",
 			"type" => "integer NOT NULL",
 			"weight" => "integer NOT NULL",
+			"primary key (itemname)",
+			"foreign key (itemname) references AuthItem (name) on delete cascade on update cascade"
 			), "");
 	}
 
